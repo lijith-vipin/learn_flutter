a2ca9152a7612d68958fdc200f75d49f8e25b7c4
lijith-vipin <lijithvipin@gmail.com>
2026-01-03 13:09:56 +0530

    Update main_detail.dart

--- lib/main_detail.dart
+++ lib/main_detail.dart
@@ -16,9 +16,11 @@ class DetailPage extends StatelessWidget {
       backgroundColor: Colors.green,
       body: BlocBuilder<NewsCubit, NewsState>(
         builder: (ctx, state) {
+          // FIXME: Properly Manage the State
           if (state.status == NewsStatus.NewsStatusLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state.status == NewsStatus.NewsStatusSuccess) {
+            // TODO: Fix this ListView Later
             return ListView.builder(
               shrinkWrap: true,
               itemCount: state.newsList?.articles.length ?? 0,
